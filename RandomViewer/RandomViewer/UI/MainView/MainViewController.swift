31b30fe84231eab0309a6ada0be714f7196b52f4
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-01-31 10:57:22 +0100

    Paragraphs done: Function random strings working

--- RandomViewer/RandomViewer/UI/MainView/MainViewController.swift
+++ RandomViewer/RandomViewer/UI/MainView/MainViewController.swift
@@ -9,21 +9,27 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // Outlets para los elementos de la vista
+    @IBOutlet weak var svTwoParagraph: UIStackView!
+    @IBOutlet weak var tfLeftParagraph: UITextView!
+    @IBOutlet weak var tfRightParagraph: UITextView!
+    
+    // Instancia del ViewModel
+    let viewModel = MainViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Llamamos a la función para generar texto aleatorio cuando la vista se carga
+        generateRandomText()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Función para generar texto aleatorio y asignarlo a los UITextView
+    func generateRandomText() {
+        // Llamamos al método del ViewModel para generar texto aleatorio
+        tfLeftParagraph.text = viewModel.generateRandomText(length: Int.random(in: 50...400))
+        tfRightParagraph.text = viewModel.generateRandomText(length: Int.random(in: 50...400))
     }
-    */
 
 }
+
--- RandomViewer/RandomViewer/UI/MainView/MainViewModel.swift
+++ RandomViewer/RandomViewer/UI/MainView/MainViewModel.swift
@@ -9,4 +9,9 @@ import Foundation
 
 class MainViewModel {
     
+    // Método para generar texto aleatorio
+    func generateRandomText(length: Int) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
+        return String((0..<length).map{ _ in letters.randomElement()! })
+    }
 }
